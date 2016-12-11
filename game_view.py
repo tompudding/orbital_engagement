ab08416b899d101b7727d1525e812bcb7d288d06
Tom Gooding <tom.pudding@gmail.com>
2016-12-11 00:20:51 +0000

    Added a scan anim

--- game_view.py
+++ game_view.py
@@ -10,10 +10,11 @@ import cmath
 G = 1
 
 class Body(object):
-    def __init__(self, pos, velocity, mass, last_pos = None):
+    def __init__(self, pos, velocity, type, mass, last_pos = None):
         self.pos = pos
         self.velocity = velocity
         self.mass = mass
+        self.type = type
         if last_pos:
             self.line_seg = drawing.Line(globals.line_buffer)
             self.line_seg.SetVertices( last_pos , self.pos, 1000 )
@@ -28,7 +29,7 @@ class Body(object):
 
         pos = self.pos + (distance_travelled * globals.units_to_pixels)
 
-        return Body(pos, velocity, self.mass, self.pos if line else None)
+        return Body(pos, velocity, self.type, self.mass, self.pos if line else None)
 
     def acc_due_to_gravity(self, body):
         vector = (self.pos - body.pos) * globals.pixels_to_units
@@ -38,6 +39,44 @@ class Body(object):
         vector = ((target.pos - self.pos).unit_vector())
         acc = ((vector * force)/ self.mass)
         self.velocity += acc
+
+    def scan_for_target(self, target, min_distance):
+        #Throw out a bunch of rays and see if any of them collide with the player's current ray
+        diff = target.pos - self.pos
+        distance, angle_to_player = cmath.polar(diff[0] + diff[1]*1j)
+        gap = math.pi/32
+        angle_guesses = [angle_to_player + i*gap for i in xrange(-16,16)]
+        #for b in self.temp_bodies:
+        #    b.line_seg.Delete()
+        #temp_bodies = []
+        period = 40000.0
+        best_time = period
+        firing_angle = None
+        for angle in angle_guesses:
+            body = self
+            v = cmath.rect(20000, angle)
+            velocity = Point(v.real, v.imag)
+            body = Body(body.pos, body.velocity + velocity, body.type, body.mass)
+            step = 1000
+            t = globals.time
+
+            while t < globals.time + period:
+                target = globals.game_view.get_obj_at_time( target.type, t )
+                if target:
+                    distance = (target.pos - body.pos).length()
+                    if distance < min_distance and (t - globals.time) < best_time:
+                        best_time = t - globals.time
+                        firing_angle = angle
+                    #body.apply_force_towards( target, 1000 )
+                body = body.step(step * globals.time_factor, globals.game_view.fixed_bodies, line = False)
+                #temp_bodies.append(body)
+                t += step
+        #for body in self.temp_bodies:
+        #    body.line_seg.SetColour( (1,1,1,1) )
+        if firing_angle is None:
+            return None
+        return firing_angle, best_time
+
 
 
 class FixedBody(Body):
@@ -77,6 +116,10 @@ class GameView(ui.RootElement):
     step_time = 500
     trail_age = 60000.0
     trail_fade = 8000.0
+    scan_duration = 500.0
+    scan_line_parts = 128
+    scan_radius = 100
+
     def __init__(self):
         self.atlas = globals.atlas = drawing.texture.TextureAtlas('tiles_atlas_0.png','tiles_atlas.txt')
         self.game_over = False
@@ -89,20 +132,21 @@ class GameView(ui.RootElement):
         self.sun = Sun()
         self.ship = Ship()
         self.enemy = Ship()
-        self.sun_body  = FixedBody( pos=Point(0,0), velocity=Point(0,0), mass=100000000 )
+        self.sun_body  = FixedBody( pos=Point(0,0), velocity=Point(0,0), type=Objects.SUN, mass=100000000 )
         self.sun.set_vertices(self.sun_body.pos)
         orbit_velocity = (math.sqrt(G * self.sun_body.mass / (100)))
         print orbit_velocity
         orbit_velocity = orbit_velocity*280
         orbit_velocity = 10000
-        self.ship_body = Body( Point(100, 0), (Point(0,-1).unit_vector()) * orbit_velocity, mass=1 )
-        self.enemy_body = Body( Point(-100, -100), (Point(-1,1).unit_vector()) * 12000, mass=1 )
+        self.ship_body = Body( Point(100, 0), (Point(0,-1).unit_vector()) * orbit_velocity, type=Objects.PLAYER, mass=1 )
+        self.enemy_body = Body( Point(-100, -100), (Point(-1,1).unit_vector()) * 12000, type=Objects.ENEMY, mass=1 )
         self.fixed_bodies = [self.sun_body]
         self.initial_state = { Objects.PLAYER : self.ship_body,
                                Objects.ENEMY  : self.enemy_body,
                                Objects.SUN    : self.sun_body }
         self.object_quads = { Objects.PLAYER : self.ship,
                               Objects.ENEMY : self.enemy }
+        self.scan_lines = [drawing.Line(globals.line_buffer) for i in xrange(self.scan_line_parts)]
         #set up the state
         self.future_state = []
         self.fill_state()
@@ -115,6 +159,7 @@ class GameView(ui.RootElement):
         self.mode = modes.Combat(self)
         self.saved_segs = { Objects.PLAYER : [],
                             Objects.ENEMY : [] }
+        self.scan_start = None
         self.StartMusic()
 
     def fill_state(self):
@@ -177,41 +222,6 @@ class GameView(ui.RootElement):
 
         #drawing.DrawAll(globals.nonstatic_text_buffer,globals.text_manager.atlas.texture)
 
-    def scan_for_player(self):
-        #Throw out a bunch of rays and see if any of them collide with the player's current ray
-        diff = self.initial_state[Objects.PLAYER].pos - self.initial_state[Objects.ENEMY].pos
-        distance, angle_to_player = cmath.polar(diff[0] + diff[1]*1j)
-        gap = math.pi/32
-        angle_guesses = [angle_to_player + i*gap for i in xrange(-16,16)]
-        #for b in self.temp_bodies:
-        #    b.line_seg.Delete()
-        self.temp_bodies = []
-        period = 40000.0
-        best_time = period
-        explosion_distance = 100
-        for angle in angle_guesses:
-            body = self.initial_state[Objects.ENEMY]
-            v = cmath.rect(20000, angle)
-            velocity = Point(v.real, v.imag)
-            body = Body(body.pos, body.velocity + velocity, body.mass)
-            step = 1000
-            t = globals.time
-
-
-            while t < globals.time + period:
-                target = self.get_obj_at_time(Objects.PLAYER, t)
-                if target:
-                    distance = (target.pos - body.pos).length()
-                    if distance < explosion_distance and (t - globals.time) < best_time:
-                        best_time = t - globals.time
-                        print angle,best_time,distance
-                    #body.apply_force_towards( target, 1000 )
-                body = body.step(step * globals.time_factor, self.fixed_bodies, line = False)
-                self.temp_bodies.append(body)
-                t += step
-        #for body in self.temp_bodies:
-        #    body.line_seg.SetColour( (1,1,1,1) )
-
     def get_obj_at_time(self, obj_type, target):
         #Get the first one before it
         closest_before = None
@@ -246,8 +256,14 @@ class GameView(ui.RootElement):
                 self.object_quads[obj_type].set_vertices( n.pos )
                 self.initial_state[obj_type] = n
 
+        if self.scan_start:
+            partial = globals.time - self.scan_start
+            if partial > self.scan_duration:
+                self.end_scan()
+            else:
+                self.draw_scan( partial / self.scan_duration )
 
-        if self.future_state[3][0] < globals.time:
+        if self.future_state[1][0] < globals.time:
             #Kill the line segments that are in the future
             for t,state in self.future_state:
                 for obj_type in Objects.mobile:
@@ -263,7 +279,9 @@ class GameView(ui.RootElement):
             self.future_state = []
             self.fill_state()
             #Now there's been an update, let's see if we can get a firing solution on the player :)
-            self.scan_for_player()
+            solution = self.initial_state[Objects.ENEMY].scan_for_target( self.initial_state[Objects.PLAYER], 100 )
+            if solution is not None:
+                print 'Enemy has firing solution go go go',solution
 
 
         for obj_type in Objects.mobile:
@@ -284,6 +302,30 @@ class GameView(ui.RootElement):
         if self.game_over:
             return
 
+    def start_scan(self):
+        #The player has started a scan, start drawing the circle
+        self.scan_start = globals.time
+        self.scan_end = globals.time + self.scan_duration
+        self.scan_start_pos = self.initial_state[Objects.PLAYER].pos
+
+    def draw_scan(self, partial):
+        r = self.scan_radius * partial
+        angles = [i*math.pi*2/self.scan_line_parts for i in xrange(self.scan_line_parts)]
+        v = cmath.rect(r, angles[0])
+        last = self.scan_start_pos + Point(v.real, v.imag)
+        for i in xrange(self.scan_line_parts):
+            angle = angles[(i + 1) % len(angles)]
+            v = cmath.rect(r, angle)
+            p = self.scan_start_pos + Point(v.real, v.imag)
+            self.scan_lines[i].SetVertices( last, p, 10000 )
+            last = p
+            intensity = 1 - partial
+            self.scan_lines[i].SetColour( (1,1,0.4,intensity))
+
+
+    def end_scan(self):
+        self.scan_start = None
+
     def GameOver(self):
         self.game_over = True
         self.mode = modes.GameOver(self)
@@ -291,14 +333,14 @@ class GameView(ui.RootElement):
     def KeyDown(self,key):
         #rejig stuff
 
-        for t, state in self.future_state:
-            state[Objects.PLAYER].line_seg.Delete()
-        for t, state in self.future_state:
-            del state[Objects.PLAYER]
+        # for t, state in self.future_state:
+        #     state[Objects.PLAYER].line_seg.Delete()
+        # for t, state in self.future_state:
+        #     del state[Objects.PLAYER]
 
-        d = 50 if key == 45 else -50
-        self.initial_state[Objects.PLAYER].velocity += Point(0,d)
-        self.fill_state_obj(Objects.PLAYER)
+        # d = 50 if key == 45 else -50
+        # self.initial_state[Objects.PLAYER].velocity += Point(0,d)
+        # self.fill_state_obj(Objects.PLAYER)
         self.mode.KeyDown(key)
 
     def KeyUp(self,key):
@@ -309,5 +351,7 @@ class GameView(ui.RootElement):
             else:
                 self.music_playing = True
                 pygame.mixer.music.set_volume(1)
+        if key == pygame.K_s:
+            self.start_scan()
         self.mode.KeyUp(key)
 
